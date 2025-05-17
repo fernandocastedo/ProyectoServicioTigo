01374e9e4e6032319cc8676409f442e5684181c0
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-05-16 23:07:30 -0400

    prueba

--- ProyectoServicioTigo/Vistas/CarouselView.xaml.cs
+++ ProyectoServicioTigo/Vistas/CarouselView.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
 
 namespace ProyectoServicioTigo.Vistas
 {
@@ -21,12 +22,41 @@ namespace ProyectoServicioTigo.Vistas
     /// </summary>
     public partial class CarouselView : UserControl
     {
+        private List<string> imagePaths = new List<string>()
+        {
+            "pack://application:,,,/Imagenes/carousel1.jpg",
+            "pack://application:,,,/Imagenes/carousel2.jpg",
+            "pack://application:,,,/Imagenes/carousel3.jpg"
+        };
+        private int currentIndex = 0;
         public CarouselView()
         {
             InitializeComponent();
-            var sb = (Storyboard)Resources["FadeStoryboard"];
-            sb.Begin();
+            ShowImage(currentIndex);
+        }
+        private void ShowImage(int index)
+        {
+            if (index >= 0 && index < imagePaths.Count)
+            {
+                BitmapImage bitmap = new BitmapImage(new System.Uri(imagePaths[index]));
+                ImageDisplay.Source = bitmap;
+            }
         }
 
+        private void PrevImage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            currentIndex--;
+            if (currentIndex < 0)
+                currentIndex = imagePaths.Count - 1;
+            ShowImage(currentIndex);
+        }
+
+        private void NextImage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            currentIndex++;
+            if (currentIndex >= imagePaths.Count)
+                currentIndex = 0;
+            ShowImage(currentIndex);
+        }
     }
 }
