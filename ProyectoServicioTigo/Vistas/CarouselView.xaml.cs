3c53385a9f61db22a77f0ebcff0dc8dd06c53151
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-05-16 23:17:10 -0400

    nuevoscambios

--- ProyectoServicioTigo/Vistas/CarouselView.xaml.cs
+++ ProyectoServicioTigo/Vistas/CarouselView.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Media.Imaging;
 
 namespace ProyectoServicioTigo.Vistas
 {
